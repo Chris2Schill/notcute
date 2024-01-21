f6a17ada0e6205b55a6e300eadcd494b69f79184
Chris Schilling <cschilling@avtsim.com>
2024-01-21 21:21:00 +0000

    Added alignment to text widget

--- include/notcute/text_widget.hpp
+++ include/notcute/text_widget.hpp
@@ -9,46 +9,75 @@ namespace notcute {
 
 class TextWidget : public Widget {
 public:
-    TextWidget(const std::string& text, Widget* parent = nullptr)
+    TextWidget(Widget* parent = nullptr): TextWidget("", parent) {}
+    TextWidget(const std::string& text, Widget* parent = nullptr, uint32_t alignment = LAY_CENTER)
         : Widget(parent)
+        ,align(alignment)
     {
         set_layout(new VBoxLayout);
-        get_layout()->set_contain(LAY_LAYOUT);
+        get_layout()->set_contain(LAY_ROW);
+        get_layout()->set_behave(LAY_CENTER | LAY_FILL);
         set_text(text);
-        // TODO REMOVE
-        // get_layout()->set_behave(LAY_LEFT|LAY_TOP);
-        get_layout()->set_behave(LAY_HFILL);
-
-        debug_set_plane_color(111,0,200);
     }
 
     void draw(ncpp::Plane* plane) override {
-        plane->putstr(text.c_str());
+
+        int cols = plane->get_dim_x();
+
+        int x = 0;
+        if (align == LAY_LEFT) {
+            x = 0;
+        }
+        else if (align == LAY_CENTER) {
+            x = cols/2 - text.size()/2;
+        }
+        else if (align == LAY_RIGHT) {
+            x = cols - text.size();
+        }
+
+        plane->set_channels(notcute::channels_from_fgbg(fg,bg));
+        plane->putstr(0,x, text.c_str());
+        log_debug(fmt::format("textwidget draw({}) plane_dim_yx({},{}) align={}, x={}",
+                    text, plane->get_dim_y(), plane->get_dim_x(), align, x));
     }
 
     void set_scrolling(bool scroll) {
         get_plane()->set_scrolling(scroll);
     }
 
+    // Separte from alignment of the widgets layout, this is for
+    // aligning the text within the plane during the draw
+    void set_align(uint32_t lay_align) {
+        align = lay_align;
+    }
+
     void set_text(const std::string& t) {
         text = t;
         int lines = std::ranges::count(t, '\n')+1;
 
-        Rect r = {};
+        Rect r = get_geometry();
         r.set_width(text.size()/lines+1);
         r.set_height(lines);
         // r.set_width(text.size());
         // r.set_height(1);
         set_geometry(r);
         redraw();
+        log_debug(fmt::format("textwidget set_text({}) resized({},{})", text, r.rows(), r.cols()));
     }
 
     std::string to_string() const override {
         return text;
     }
 
+    void set_fg(Color c) { fg = c; }
+    void set_bg(Color c) { bg = c; }
+
 private:
+    Color fg = {255,255,255,NCALPHA_OPAQUE};
+    Color bg = {000,000,000,NCALPHA_TRANSPARENT};
+
     std::string text;
+    uint32_t align = LAY_CENTER;
 };
 
 }
