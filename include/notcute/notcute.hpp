070db05810a5dd756f1c3021c2457e0f3a2477d7
Chris Schilling <cschilling@avtsim.com>
2024-01-29 17:52:44 +0000

    Added helper method to check for widget mem leaks

--- include/notcute/notcute.hpp
+++ include/notcute/notcute.hpp
@@ -5,3 +5,13 @@
 #include "logger.hpp"
 #include "renderer.hpp"
 #include "text_widget.hpp"
+
+
+namespace notcute {
+
+inline void report_closing_diagnostic() {
+    log_debug("Shutting Down");
+    log_debug(fmt::format("Alive Widgets = {}", Widget::alive_widgets));
+}
+
+}
